f5ad0dacbf49cf2f37b4380dec960096f2cd07f2
Yegor-own <68266911+Yegor-own@users.noreply.github.com>
2020-12-25 19:15:21 +0500

    добавлена кластеризация

--- index.php
+++ index.php
@@ -22,6 +22,8 @@ $num_rows = mysqli_num_rows($cameras);
     <title>TFind</title>
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A==" crossorigin=""/>
     <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js" integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA==" crossorigin=""></script>
+    <link rel="stylesheet" href="https://unpkg.com/leaflet.markercluster@1.4.1/dist/MarkerCluster.css" />
+    <link rel="stylesheet" href="https://unpkg.com/leaflet.markercluster@1.4.1/dist/MarkerCluster.Default.css" />
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
 
     <style>
@@ -47,6 +49,7 @@ $num_rows = mysqli_num_rows($cameras);
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+    <script src="https://unpkg.com/leaflet.markercluster@1.4.1/dist/leaflet.markercluster.js"></script>
     <script>
         let map = L.map('map').setView([56.8519000, 60.6122000], 13);
         L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -59,31 +62,46 @@ $num_rows = mysqli_num_rows($cameras);
     <?php
     if ($c) { ?>
         <script>
-            let loc = '<?php echo $loc; ?>';
-            let loc2 = '<?php echo $loc2; ?>';
-            let adres = '<?php echo $adres; ?>';
-            let descr = '<?php echo $desc; ?>';
+            let loc = '<?php echo $loc; ?>'
+            let loc2 = '<?php echo $loc2; ?>'
+            let adres = '<?php echo $adres; ?>'
+            let descr = '<?php echo $desc; ?>'
             L.marker([loc, loc2]).addTo(map)
                 .bindPopup(adres + "<br><br>" + descr)
                 .openPopup();
         </script>
     <?php } else {
         unset($cam);
+        ?>
+        <script>
+        let loc
+        let loc2
+        let adres
+        let descr
+        let markers = new L.MarkerClusterGroup()
+        </script>
+        <?php
         while ($cam = mysqli_fetch_assoc($cameras)) {
             ?>
             <script>
             if (i != counter){
-                i++;
-                let loc = '<?php echo $cam['location']; ?>';
-                let loc2 = '<?php echo $cam['location2']; ?>';
-                let adres = '<?php echo $cam['addres']; ?>';
-                let descr = '<?php echo $cam['descr']; ?>';
-                L.marker([loc, loc2]).addTo(map)
-                    .bindPopup(adres + "<br><br>" + descr)
-                    .openPopup();
+                i++
+                loc = '<?php echo $cam['location']; ?>'
+                loc2 = '<?php echo $cam['location2']; ?>'
+                adres = '<?php echo $cam['addres']; ?>'
+                descr = '<?php echo $cam['descr']; ?>'
+                markers.addLayer(
+                    L.marker([loc, loc2])
+                        // .bindPopup(adres + "<br><br>" + descr)
+                        // .openPopup()
+                )
             }
             </script>
-        <?php } 
+        <?php } ?>
+        <script>
+        map.addLayer(markers)
+        </script>
+    <?php
     }?>
 </body>
 
